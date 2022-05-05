e3f56de7c72b053c603b3eaf426cbf889882efcb
Albin Dahlén <b21albda@student.his.se>
2022-05-05 14:39:49 +0200

    Implemented country

--- app/src/main/java/com/example/persistence/Country.java
+++ app/src/main/java/com/example/persistence/Country.java
@@ -1,4 +1,32 @@
 package com.example.persistence;
 
 public class Country {
+
+    private final int id;
+    private String name;
+    private String capital;
+    private int population;
+
+    public Country(int id, String name, String capital, int population) {
+        this.id = id;
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
 }
