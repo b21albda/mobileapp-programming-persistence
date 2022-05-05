4143f242c7ff2a23cfac7db6cbd73d827c5e7b89
Albin Dahlén <b21albda@student.his.se>
2022-05-05 14:26:04 +0200

    Implemented DatabaseTables class

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -1,4 +1,22 @@
 package com.example.persistence;
 
 public class DatabaseTables {
+
+    static class COUNTRY {
+        static final String TABLE_NAME = "country";
+        static final String COLUMN_NAME_ID = "id";
+        static final String COLUMN_NAME_NAME = "name";
+        static final String COLUMN_NAME_CAPITAL = "capital";
+        static final String COLUMN_NAME_POPULATION = "population";
+    }
+
+    static final String SQL_CREATE_TABLE_COUNTRY =
+            "CREATE TABLE " + COUNTRY.TABLE_NAME + " (" +
+                    COUNTRY.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
+                    COUNTRY.COLUMN_NAME_NAME + " TEXT," +
+                    COUNTRY.COLUMN_NAME_CAPITAL + " TEXT," +
+                    COUNTRY.COLUMN_NAME_POPULATION + " INT)";
+
+    static final String SQL_DELETE_TABLE_COUNTRY =
+            "DROP TABLE IF EXISTS " + COUNTRY.TABLE_NAME;
 }
